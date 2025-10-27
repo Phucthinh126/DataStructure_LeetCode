550f549c21a0884fcb1acdca835aac69a44901d0
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2025-10-27 22:33:30 +0700

    final leetCode 912 using merge sort

--- src/leetCode/LeetCode_912_SortAnArray.java
+++ src/leetCode/LeetCode_912_SortAnArray.java
@@ -23,4 +23,54 @@ public class LeetCode_912_SortAnArray {
      * Solution: merge sort
      */
 
+    public static int[] sortArray(int[] nums) {
+        return merge_Sort(0, nums.length - 1, nums);
+    }
+
+    public static int[] merge_Sort(int left, int right, int[] nums) {
+        if (left > right) {
+            return new int[0];
+        }
+        if (left == right) {
+            int[] singleElement = { nums[left] };
+            return singleElement;
+        }
+        int mid = (left + right) / 2;
+        int[] arr1 = merge_Sort(left, mid, nums);
+        int[] arr2 = merge_Sort(mid + 1, right, nums);
+        int[] result = merge(arr1, arr2);
+        return result;
+
+    }
+
+    public static int[] merge(int[] arr1, int[] arr2) {
+        int n = arr1.length + arr2.length;
+        int[] result = new int[n];
+        int i = 0, i1 = 0, i2 = 0;
+        while (i < n) {
+            if (i1 < arr1.length && i2 < arr2.length) {
+                if (arr1[i1] <= arr2[i2]) {
+                    result[i] = arr1[i1];
+                    i++;
+                    i1++;
+                } else {
+                    result[i] = arr2[i2];
+                    i++;
+                    i2++;
+                }
+            } else {
+                if (i1 < arr1.length) {
+                    result[i] = arr1[i1];
+                    i++;
+                    i1++;
+                } else {
+                    result[i] = arr2[i2];
+                    i++;
+                    i2++;
+                }
+            }
+        }
+        return result;
+    }
+
 }
