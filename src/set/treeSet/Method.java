584315c19f88e52e887bd1a2d71880efb9fa9f7d
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2025-11-24 01:25:10 +0700

    method TreeSet

---
+++ src/set/treeSet/Method.java
@@ -0,0 +1,21 @@
+package set.treeSet;
+
+import java.util.TreeSet;
+
+public class Method {
+
+    public static void main(String[] args) {
+        // tao TreeSet
+        TreeSet<Integer> set = new TreeSet<>();
+        
+        // add
+        set.add(3);
+        set.add(2);
+        set.add(0);
+        set.add(9);
+        set.add(6);
+
+        
+
+    }
+}
