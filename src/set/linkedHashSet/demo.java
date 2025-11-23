79980f5bbb622f38a672515c9161ab52bbb33dbd
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2025-11-24 01:23:53 +0700

    demo linkedHashSet

---
+++ src/set/linkedHashSet/demo.java
@@ -0,0 +1,16 @@
+package set.linkedHashSet;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class demo {
+    // khai trien linkedHashSet
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>();
+        set.add("Thinh");
+        set.add("Huynh");
+        set.add("Phuc");
+        System.out.println(set);
+    }
+
+}
